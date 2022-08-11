67f82e30ed9cf1014e85900d6ad7f37cfd4f79af
Zarivate <Zarivate1@gmail.com>
2022-08-11 17:39:21 -0500

    Added experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -9,18 +9,82 @@ import github from "../assets/github.png";
 import tailwind from "../assets/tailwind.png";
 
 const Experience = () => {
+  const technologies = [
+    {
+      id: 1,
+      src: html,
+      title: "HTML",
+      style: "shadow-orange-500",
+    },
+    {
+      id: 2,
+      src: css,
+      title: "CSS",
+      style: "shadow-blue-500",
+    },
+    {
+      id: 3,
+      src: javascript,
+      title: "JavaScript",
+      style: "shadow-yellow-500",
+    },
+    {
+      id: 4,
+      src: reactImage,
+      title: "React",
+      style: "shadow-blue-600",
+    },
+    {
+      id: 5,
+      src: tailwind,
+      title: "Tailwind",
+      style: "shadow-sky-400",
+    },
+    {
+      id: 6,
+      src: nextjs,
+      title: "Next JS",
+      style: "shadow-white",
+    },
+    {
+      id: 7,
+      src: graphql,
+      title: "GraphQL",
+      style: "shadow-pink-400",
+    },
+    {
+      id: 8,
+      src: github,
+      title: "GitHub",
+      style: "shadow-gray-400",
+    },
+  ];
+
   return (
-    <div>
-      <div>
+    <div
+      name="experience"
+      className="bg-gradient-to-b from-gray-800 to to-black w-full h-fit pt-10"
+    >
+      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div>
-          <p>Experience</p>
-          <p>The technologies I've worked with</p>
+          <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
+            Experience
+          </p>
+          <p className="py-6">The technologies I've worked with</p>
         </div>
-        <div>
-          <div>
-            <img src={html} alt="" />
-            <p>Title here</p>
-          </div>
+        {/* Mobile device displays will have a two column grid. For even smaller devices the "sm:" will make it so there's padding on left and right while none for bigger devices */}
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+          {/* The technologies array is looped through after this div so that the grid effect can be applied */}
+          {technologies.map(({ id, src, title, style }) => (
+            // Parentheses needed here instead of brackets since returning JSX
+            <div
+              key={id}
+              className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
+            >
+              <img src={src} alt="" className="w-20 mx-auto" />
+              <p className="mt-4">{title}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
